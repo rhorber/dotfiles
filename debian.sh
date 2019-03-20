a98e37edc5f0571f5650cf8c3cb2374e11be3627
Raphael Horber <raphael.horber@gmail.com>
2019-03-20 20:59:57 +0100

    Debian: Removed creation of .gitconfig

--- debian.sh
+++ debian.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 
-# *** Exeucte actions which require user input at the start ***
-# Ask for Git user email
-read -p "Enter Git user's email address (leave empty for skipping .gitconfig): " gitUserEmail
-
-
 # *** inputrc ***
 cp ./shell/debian.inputrc ~/.inputrc
 
