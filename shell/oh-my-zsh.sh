82d8686cf108a1ef31bd3a64f74baeb4271db9fe
Raphael Horber <raphael.horber@gmail.com>
2021-09-01 23:17:48 +0200

    OMZ: Use powerlevel10k and more plugins

--- shell/oh-my-zsh.sh
+++ shell/oh-my-zsh.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 
 # ** Install **
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 
 # ** Config **
@@ -18,8 +18,8 @@ sed -ne "1,$foo p" $bak >> $cfg
 
 # add new theme
 echo "#ZSH_THEME=\"robbyrussell\"" >> $cfg
-echo "PRIMARY_FG=white" >> $cfg
-echo "ZSH_THEME=\"agnoster\"" >> $cfg
+echo "POWERLEVEL9K_MODE='nerdfont-complete'" >> $cfg
+echo "ZSH_THEME='powerlevel10k/powerlevel10k'" >> $cfg
 
 # add until plugins
 let 'foo = tmp + 1'
@@ -28,14 +28,9 @@ tmp=`grep -nE "^plugins=\($" $bak | cut -d: -f1`
 if [ "${tmp}" != "" ]; then
     sed -ne "$foo,$tmp p" $bak >> $cfg
 
-    # set desired plugins
-    echo "  colored-man-pages" >> $cfg
-    echo "  git" >> $cfg
-    echo "  zsh-syntax-highlighting" >> $cfg
-
-    # add remainder
+    # calculate remainder
     tmp=`grep -nE "^)$" $bak | cut -d: -f1`
-    sed -ne "$tmp,$ p" $bak >> $cfg
+    let 'tmp = tmp + 1'
 else
     tmp=`grep -nE "^plugins=\(" $bak | cut -d: -f1`
     let 'tmp = tmp - 1'
@@ -43,42 +38,56 @@ else
 
     # set desired plugins
     echo "plugins=(" >> $cfg
-    echo "  colored-man-pages" >> $cfg
-    echo "  git" >> $cfg
-    echo "  zsh-syntax-highlighting" >> $cfg
-    echo ")" >> $cfg
 
-    # add remainder
+    # calculate remainder
     let 'tmp = tmp + 2'
-    sed -ne "$tmp,$ p" $bak >> $cfg
 fi
 
+# set desired plugins
+echo "  colored-man-pages" >> $cfg
+echo "  docker" >> $cfg
+echo "  docker-compose" >> $cfg
+echo "  git" >> $cfg
+echo "  npm" >> $cfg
+echo "  yarn" >> $cfg
+echo "  zsh-autosuggestions" >> $cfg
+echo "  zsh-syntax-highlighting" >> $cfg
+echo ")" >> $cfg
+
+# add remainder
+sed -ne "$tmp,$ p" $bak >> $cfg
+
+# enable waiting dots
+sed -i 's/# COMPLETION_WAITING_DOTS="true"/COMPLETION_WAITING_DOTS="true"/' $cfg
+
+
+# ** Powerlevel10k Theme **
+cd ~/.oh-my-zsh/custom/themes
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git
+
+# ** Auto-Suggestions Plugin **
+cd ~/.oh-my-zsh/custom/plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions.git
 
 # ** Syntax Highlighting Plugin **
 cd ~/.oh-my-zsh/custom/plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 
 
-# ** Current Agnoster Theme **
-cd ~/.oh-my-zsh/custom/themes
-git clone https://github.com/agnoster/agnoster-zsh-theme.git
-mv agnoster-zsh-theme/agnoster.zsh-theme .
-rm -rf agnoster-zsh-theme
-
-
 # ** Customize Theme **
-agnoster="$HOME/.oh-my-zsh/custom/agnoster-customization.zsh"
-echo "# Customize agnoster: Remove context and display time" > $agnoster
-echo "custom_date () {" >> $agnoster
-echo "  prompt_segment 'black' \$PRIMARY_FG \" \`date +'%H:%M:%S'\` \"" >> $agnoster
-echo "}" >> $agnoster
-echo "" >> $agnoster
-echo "AGNOSTER_PROMPT_SEGMENTS[2]=custom_date" >> $agnoster
-echo "AGNOSTER_PROMPT_SEGMENTS[3]=prompt_context" >> $agnoster
-echo "DEFAULT_USER=raphael" >> $agnoster
-echo "" >> $agnoster
+powerlevel10k="$HOME/.oh-my-zsh/custom/powerlevel10k-customization.zsh"
+cat > $powerlevel10k << POWERLEVEL10K
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(status time dir dir_writable vcs)
+POWERLEVEL9K_DISABLE_RPROMPT=true
+POWERLEVEL9K_STATUS_OK=false
+POWERLEVEL9K_SHORTEN_DIR_LENGTH=3
 
-echo "" >> $cfg
-echo "# Load agnoster customization" >> $cfg
-echo "source ${agnoster}" >> $cfg
-echo "" >> $cfg
+POWERLEVEL9K_TIME_FOREGROUND='white'
+POWERLEVEL9K_TIME_BACKGROUND='236'
+
+POWERLEVEL9K_DIR_HOME_FOREGROUND='white'
+POWERLEVEL9K_DIR_HOME_SUBFOLDER_FOREGROUND='white'
+POWERLEVEL9K_DIR_DEFAULT_FOREGROUND='white'
+POWERLEVEL9K_DIR_ETC_FOREGROUND='226'
+
+POWERLEVEL10K
