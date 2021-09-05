44cbf7cd101c6a2070cc751abe425c1debe9caf1
Raphael Horber <raphael.horber@gmail.com>
2021-09-05 16:26:12 +0200

    Use same indent in all scripts

--- aliases/advanced-coreutils.sh
+++ aliases/advanced-coreutils.sh
@@ -1,8 +1,8 @@
 if [ -x "/usr/local/bin/cp" ]; then
-    alias cp='/usr/local/bin/cp -gR'
+  alias cp='/usr/local/bin/cp -gR'
 fi
 
 if [ -x "/usr/local/bin/mv" ]; then
-    alias mv='/usr/local/bin/mv -g'
+  alias mv='/usr/local/bin/mv -g'
 fi
 
--- shell/debian.aliases.sh
+++ shell/debian.aliases.sh
@@ -2,46 +2,46 @@
 
 # Working directory
 if [ "${PWD##*/}" != "aliases" ]; then
-	cd ../aliases || exit
+  cd ../aliases || exit
 fi
 
 # Copy aliases
 rm "$HOME"/.aliases/*
 if [ ! -d "$HOME/.aliases/" ]; then
-	mkdir "$HOME/.aliases"
+  mkdir "$HOME/.aliases"
 fi
 cp ./*.sh "$HOME/.aliases/"
 
 # Bash
 if command -v bash &> /dev/null; then
-    cp ./*.bash "$HOME/.aliases/"
-    if ! grep -F "# Load our aliases" "$HOME/.bashrc" &> /dev/null; then
-        {
-            echo ""
-            echo "# Load our aliases"
-            echo "if [ -d ~/.aliases/ ]; then"
-            echo "    find ~/.aliases -type f -name '*.sh' -exec bash -c 'source \"\$0\"' {} ';'"
-            echo "    find ~/.aliases -type f -name '*.bash' -exec bash -c 'source \"\$0\"' {} ';'"
-            echo "fi"
-            echo ""
-        } >> "$HOME/.bashrc"
-    fi
+  cp ./*.bash "$HOME/.aliases/"
+  if ! grep -F "# Load our aliases" "$HOME/.bashrc" &> /dev/null; then
+    {
+      echo ""
+      echo "# Load our aliases"
+      echo "if [ -d ~/.aliases/ ]; then"
+      echo "    find ~/.aliases -type f -name '*.sh' -exec bash -c 'source \"\$0\"' {} ';'"
+      echo "    find ~/.aliases -type f -name '*.bash' -exec bash -c 'source \"\$0\"' {} ';'"
+      echo "fi"
+      echo ""
+    } >> "$HOME/.bashrc"
+  fi
 fi
 
 # ZSH
 if command -v zsh &> /dev/null; then
-    cp ./*.zsh "$HOME/.aliases/"
-    if ! grep -F "# Load our aliases" "$HOME/.zshrc" &> /dev/null; then
-        {
-            echo ""
-            echo "# Load our aliases"
-            echo "if [ -d ~/.aliases/ ]; then"
-            echo "    find ~/.aliases -type f -name '*.sh' -exec bash -c 'source \"\$0\"' {} ';'"
-            echo "    find ~/.aliases -type f -name '*.zsh' -exec bash -c 'source \"\$0\"' {} ';'"
-            echo "fi"
-            echo ""
-        } >> "$HOME/.zshrc"
-    fi
+  cp ./*.zsh "$HOME/.aliases/"
+  if ! grep -F "# Load our aliases" "$HOME/.zshrc" &> /dev/null; then
+    {
+      echo ""
+      echo "# Load our aliases"
+      echo "if [ -d ~/.aliases/ ]; then"
+      echo "    find ~/.aliases -type f -name '*.sh' -exec bash -c 'source \"\$0\"' {} ';'"
+      echo "    find ~/.aliases -type f -name '*.zsh' -exec bash -c 'source \"\$0\"' {} ';'"
+      echo "fi"
+      echo ""
+    } >> "$HOME/.zshrc"
+  fi
 fi
 
 # Change owner
@@ -50,13 +50,13 @@ chown "$USER": "$HOME"/.aliases/*
 # Copy binaries
 # TODO: Needs sudo...
 if [ -f "../bin/cp.bin" ]; then
-    cp ../bin/cp.bin /usr/local/bin/cp
-    chown "$USER": /usr/local/bin/cp
-    chmod 0755 /usr/local/bin/cp
+  cp ../bin/cp.bin /usr/local/bin/cp
+  chown "$USER": /usr/local/bin/cp
+  chmod 0755 /usr/local/bin/cp
 fi
 
 if [ -f "../bin/mv.bin" ]; then
-    cp ../bin/mv.bin /usr/local/bin/mv
-    chown "$USER": /usr/local/bin/mv
-    chmod 0755 /usr/local/bin/mv
+  cp ../bin/mv.bin /usr/local/bin/mv
+  chown "$USER": /usr/local/bin/mv
+  chmod 0755 /usr/local/bin/mv
 fi
