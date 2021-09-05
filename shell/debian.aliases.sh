437651dcecb7a84a1d75c2ec01b4bc17a64fba5f
Raphael Horber <raphael.horber@gmail.com>
2021-09-05 15:07:47 +0200

    Aliases: Fix working dir and better loop

--- shell/debian.aliases.sh
+++ shell/debian.aliases.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
-
 # Working directory
 if [ "${PWD##*/}" != "aliases" ]; then
-	cd aliases || exit
+	cd ../aliases || exit
 fi
-
 
 # Copy aliases
 rm "$HOME"/.aliases/*
-if [ ! -d "$HOME/.aliases" ]; then
+if [ ! -d "$HOME/.aliases/" ]; then
 	mkdir "$HOME/.aliases"
 fi
-cp ./* "$HOME/.aliases"
-chown "$USER": "$HOME"/.aliases/*
+cp ./*.sh "$HOME/.aliases/"
 
+# Change owner
+chown "$USER": "$HOME"/.aliases/*
 
 # Copy binaries
 # TODO: Needs sudo...
@@ -23,35 +22,34 @@ cp ../bin/mv_v8.32.amd64.bin /usr/local/bin/mv
 chown "$USER": /usr/local/bin/{cp,mv}
 chmod 0755 /usr/local/bin/{cp,mv}
 
-
 # Bash
 if command -v bash &> /dev/null; then
+    cp ./*.bash "$HOME/.aliases/"
     if ! grep -F "# Load our aliases" "$HOME/.bashrc" &> /dev/null; then
         {
             echo ""
             echo "# Load our aliases"
             echo "if [ -d ~/.aliases/ ]; then"
-            echo "    for f in ~/.aliases/*.sh; do source \"\$f\"; done"
-            echo "    for f in ~/.aliases/*.bash; do source \"\$f\"; done"
+            echo "    find ~/.aliases -type f -name '*.sh' -exec bash -c 'source \"\$0\"' {} ';'"
+            echo "    find ~/.aliases -type f -name '*.bash' -exec bash -c 'source \"\$0\"' {} ';'"
             echo "fi"
             echo ""
         } >> "$HOME/.bashrc"
     fi
 fi
 
-
 # ZSH
 if command -v zsh &> /dev/null; then
+    cp ./*.zsh "$HOME/.aliases/"
     if ! grep -F "# Load our aliases" "$HOME/.zshrc" &> /dev/null; then
         {
             echo ""
             echo "# Load our aliases"
             echo "if [ -d ~/.aliases/ ]; then"
-            echo "    for f in ~/.aliases/*.sh; do source \"\$f\"; done"
-            echo "    for f in ~/.aliases/*.zsh; do source \"\$f\"; done"
+            echo "    find ~/.aliases -type f -name '*.sh' -exec bash -c 'source \"\$0\"' {} ';'"
+            echo "    find ~/.aliases -type f -name '*.zsh' -exec bash -c 'source \"\$0\"' {} ';'"
             echo "fi"
             echo ""
         } >> "$HOME/.zshrc"
     fi
 fi
-
