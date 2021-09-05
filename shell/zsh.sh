a83b932e9173d2a5f115e3f3f516915aaeb4dbd1
Raphael Horber <raphael.horber@gmail.com>
2021-09-05 21:00:36 +0200

    Shell: Add comment before ZSH options

--- shell/zsh.sh
+++ shell/zsh.sh
@@ -3,6 +3,8 @@
 # Option rmstarsilent
 if ! grep -F "rmstarsilent" "$HOME/.zshrc" &> /dev/null; then
   {
+    echo ""
+    echo "# Custom options"
     echo "setopt rmstarsilent"
     echo ""
   } >> "$HOME/.zshrc"
