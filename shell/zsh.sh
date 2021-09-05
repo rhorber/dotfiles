983db6842c0768dc2b641223a945f4583c5058c8
Raphael Horber <raphael.horber@gmail.com>
2021-09-05 15:52:36 +0200

    Shell: Add zsh customization

---
+++ shell/zsh.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Option rmstarsilent
+if ! grep -F "rmstarsilent" "$HOME/.zshrc" &> /dev/null; then
+    {
+        echo "setopt rmstarsilent"
+        echo ""
+    } >> "$HOME/.zshrc"
+fi
