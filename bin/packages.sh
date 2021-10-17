b469d6caa3b32cb5fe7c5096d323776c65ecb75e
Raphael Horber <raphael.horber@gmail.com>
2021-10-17 14:48:04 +0200

    Packages: Add `apt-transport-https`

--- bin/packages.sh
+++ bin/packages.sh
@@ -6,8 +6,8 @@ if [ "$(id -u)" -ne 0 ]; then
 fi
 
 # System tools (task "standard system utilities")
-apt-get -y install bash-completion ca-certificates distro-info-data iso-codes lsb-release \
-  man-db netcat-traditional openssl traceroute wget
+apt-get -y install apt-transport-https bash-completion ca-certificates distro-info-data \
+  iso-codes lsb-release man-db netcat-traditional openssl traceroute wget
 
 # Our tools
 apt-get -y install curl htop iftop pv screenfetch ssh tree vim
