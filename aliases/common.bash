4702b8fa54ca6ca5370817564be2c92ef4346fc4
Raphael Horber <raphael.horber@gmail.com>
2022-03-12 21:53:45 +0100

    Alisases: Move zsh aliases into shell aliases
    The aliases in the zsh file were also in the bash file. Therefore,
    move them into the shell file and remove them from the bash file.

--- aliases/common.bash
+++ aliases/common.bash
@@ -6,11 +6,3 @@ alias ls='ls $LS_OPTIONS'
 alias ll='ls $LS_OPTIONS -l'
 alias l='ls $LS_OPTIONS -la'
 alias dir='ls $LS_OPTIONS'
-
-# Default to human readable figures
-alias df='df -h'
-alias du='du -h'
-
-alias ping='ping -c4'
-
-alias bat='batcat'
