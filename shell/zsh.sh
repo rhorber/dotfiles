8fd17d6c1fb36625d13ed862a974b6e893612a23
Raphael Horber <raphael.horber@gmail.com>
2022-03-23 12:47:04 +0100

    Shell: Install `zsh`, if not already installed

--- shell/zsh.sh
+++ shell/zsh.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+
+# ** Install **
+if ! command -v zsh &> /dev/null; then
+  if [ "$(whoami)" = "root" ]; then
+    SUDO=""
+  elif command -v sudo &> /dev/null; then
+    SUDO="sudo"
+  else
+    echo "$(printf '\033[33m')Zsh is not installed.$(printf '\033[0m') Please install zsh first."
+    exit 1
+  fi
+
+  ${SUDO} apt-get -y install zsh
+fi
+
+
 # Option rmstarsilent
 if ! grep -F "rmstarsilent" "$HOME/.zshrc" &> /dev/null; then
   {
